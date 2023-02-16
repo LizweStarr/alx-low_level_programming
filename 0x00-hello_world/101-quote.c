8ab6b5731d5b1791a6633ffc338ea5084723ab88
LizweStarr <manciyalindokuhle30@gmail.com>
2023-02-16 14:56:40 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
-#include <unistd.h>
-
+#include<unistd.h>
 /**
- * main - UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity
+ * * main - prints the standrad error sandwiched around the  standard output
  *
- * Return: Always 0 (Success)
+ * Return: returns 1
  */
 int main(void)
 {
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
-
